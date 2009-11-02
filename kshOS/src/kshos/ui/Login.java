eee3d3a0d9ccc1fd3c665fc64669f2b10665eebf
hauzi.m <hauzi.m@7d74cbbc-c472-11de-9797-47652b86970f>
2009-11-02 12:58:28 +0000

    * empty managers added * basic login window added

---
+++ kshOS/src/kshos/ui/Login.java
@@ -0,0 +1,65 @@
+package kshos.ui;
+
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ * Basic loggin window.
+ * Offers only user name selection, no password validation.
+ *
+ * @author <a href="mailto:hauzi.m@gmail.com">Miroslav Hauser</a>
+ * @version 0.01, 1.11.2009
+ */
+public class Login extends JFrame {
+
+    private JLabel Luser = null;        // Luser :-] ... label
+    private JTextField TFuser = null;   // Test field for user name
+    private JButton BTok = null;        // OK button
+    private JButton BTstorno = null;    // Strono button
+
+    public Login() {
+
+        initComponents();
+        setVisible(true);
+    }
+
+    /**
+     * Initializes all window components.
+     */
+    private void initComponents() {
+
+        this.setTitle("Login");
+        this.setSize(new Dimension(320, 240));
+        this.setResizable(false);
+        this.addWindowListener(new WindowAdapter() {
+			// if user clicks on the closing cross
+                        @Override
+			public void windowClosing(WindowEvent we) {
+				close();
+			}
+		});
+
+        Luser = new JLabel("User name: ");
+
+        TFuser = new JTextField();
+
+        BTok = new JButton("OK");
+        BTok.addActionListener(null);
+
+        BTstorno = new JButton("Storno");
+        BTstorno.addActionListener(null);
+    }
+
+    /**
+     * Close actual loggin window. It closes all consoles too.
+     */
+    public synchronized void close() {
+        this.dispose();
+    }
+
+}
