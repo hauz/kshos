e2cc5074a66b5d4c18577e7177a766c4141d6b72
zdenek4 <zdenek4@7d74cbbc-c472-11de-9797-47652b86970f>
2009-11-24 16:06:09 +0000

    * compile.bat updated

--- trunk/kshOS_script/compile.bat
+++ trunk/kshOS_script/compile.bat
@@ -1,2 +1,5 @@
 mkdir build
 javac -d build -classpath lib\antlr-3.2.jar -sourcepath src src\kshos\*.java
+javac -d build -classpath lib\antlr-3.2.jar -sourcepath src src\kshos\command\*.java
+mkdir build\kshos\config
+copy src\kshos\config build\kshos\config
