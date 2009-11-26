f8901094ef7686b56c2cab03f95cecb56ece45a7
hauzi.m@gmail.com <hauzi.m@gmail.com@7d74cbbc-c472-11de-9797-47652b86970f>
2009-11-26 22:31:15 +0000

    * Infomessage added

--- kshOS_script/compile.bat
+++ kshOS_script/compile.bat
@@ -3,3 +3,11 @@ javac -d build -classpath lib\antlr-3.2.jar -sourcepath src src\kshos\*.java
 javac -d build -classpath lib\antlr-3.2.jar -sourcepath src src\kshos\command\*.java
 mkdir build\kshos\config
 copy src\kshos\config build\kshos\config
+
+echo
+echo "#################################"
+echo "Accepted user names:"
+echo " * hauz"
+echo " * k4chn1k"
+echo " * sysek"
+echo " * guest"
